d67e8b2aeb2a3c9166b2dac0ad28d59107bedbc8
elorajm <eloraathias@gmail.com>
2025-11-01 15:05:14 -0500

    u

--- BlazorPizzaApp/PizzaApp/obj/Debug/net9.0/PizzaApp.AssemblyInfo.cs
+++ BlazorPizzaApp/PizzaApp/obj/Debug/net9.0/PizzaApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PizzaApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b55fb36e6b12a1645eb852e6223e06be051995c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("PizzaApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PizzaApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
